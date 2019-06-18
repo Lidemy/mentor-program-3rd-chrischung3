6b74b2534d361596d8362fa236e4e490237baa4a
Chris Chung <chrischung@Chrisde-MacBook-Air.local>
2019-06-18 22:50:12 +0800

    week2_hw_done

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
-  return str;
+  let newarray = [];
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    newarray.push(str[i]);
+  }
+  newarray = newarray.join('');
+  console.log(newarray);
 }
 
 reverse('hello');
